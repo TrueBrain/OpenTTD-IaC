3a9886837e07f833ef1dc9204c0608f721c9020f
Patric Stout <truebrain@openttd.org>
2018-12-29 22:55:26 +0100

    Fix: more folders were incorrect

--- run.sh
+++ run.sh
@@ -12,7 +12,8 @@ sleep 1
 
 # Make sure openttd-base is updated (it is not monitored by deployer, and can
 # only ever change if this container is redeployed anyway).
-helm dep update charts/openttd-base
-helm upgrade --force --namespace global --install global-openttd-base -f config/global/global.yaml -f config/global/openttd-base.yaml charts/openttd-base
+helm dep update /charts/openttd-base
+helm upgrade --force --namespace global --install global-openttd-base -f /config/global/global.yaml -f /config/global/openttd-base.yaml /charts/openttd-base
 
+cd /code
 python -m deployer
