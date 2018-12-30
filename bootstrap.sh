55fdd4b88ea4c7149546bad8888fcb7102495d6a
Patric Stout <truebrain@openttd.org>
2018-12-30 13:00:20 +0100

    Fix: several bootstrap and startup issues
    - Helm does a piss-poor job in maintaining CRDs. So we now do it on bootstrap ourself
    - Install openttd-base needs additional permissions. So we now do it on bootstrap
    - Minor typos and dangling empty files
    - RBAC permissions were all wrong

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,33 @@
 set -e
 
 kubectl apply -f config/namespaces.yaml
-kubectl apply -f config/deployer-service-account.yaml
+kubectl apply -f config/crd-charts.yaml
+kubectl apply -f config/sa-deployer.yaml
 
-helm upgrade --force --namespace global --install global-openttd-iac -f config/global/global.yaml -f config/global/openttd-iac.yaml charts/openttd-iac
+# External CRDs; helm doesn't support CRDs that well, so we apply it manually
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/master/deploy/manifests/00-crds.yaml
+
+# Install openttd-base. This is the only package not monitored/deployed by
+# deployer.This is mainly because it needs a ton of access to do so, which is
+# unwise to assign to a single pod.
+helm dep update charts/openttd-base
+helm upgrade \
+    --force \
+    --namespace global \
+    --set cert-manager.createCustomResource=false \
+    --install global-openttd-base \
+    -f config/global/global.yaml \
+    -f config/global/openttd-base.yaml \
+    charts/openttd-base
+
+if [ -z "$(helm ls -q | grep global-openttd-iac)" ]; then
+    # Deploy openttd-iac for the first time. This contains the deployer, and will
+    # take over from there.
+    helm upgrade \
+        --force \
+        --namespace global \
+        --install global-openttd-iac \
+        -f config/global/global.yaml \
+        -f config/global/openttd-iac.yaml \
+        charts/openttd-iac
+fi
--- run.sh
+++ run.sh
@@ -10,10 +10,5 @@ tiller --history-max 10 --storage secret &
 export HELM_HOST=":44134"
 sleep 1
 
-# Make sure openttd-base is updated (it is not monitored by deployer, and can
-# only ever change if this container is redeployed anyway).
-helm dep update /charts/openttd-base
-helm upgrade --force --namespace global --install global-openttd-base -f /config/global/global.yaml -f /config/global/openttd-base.yaml /charts/openttd-base
-
 cd /code
 python -m deployer
