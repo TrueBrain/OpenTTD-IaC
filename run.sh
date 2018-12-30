386084645ee9689b23704a15fc3d5534376b09e0
Patric Stout <truebrain@openttd.org>
2018-12-30 16:15:32 +0100

    Fix: make sure tiller is terminated as soon as the deployer is

--- run.sh
+++ run.sh
@@ -3,10 +3,15 @@
 # Startup script for the container.
 #
 
-set -e
-
 # Start a local tiller and make sure the rest connect to it
 TILLER_NAMESPACE=global tiller --history-max 10 --storage secret &
+tiller_pid=$!
 sleep 1
 
 HELM_HOST=":44134" python -m deployer
+exit_code=$?
+
+# If the deployer stops, stop tiller too
+kill ${tiller_pid}
+
+exit ${exit_code}
