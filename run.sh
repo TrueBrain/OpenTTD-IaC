200efb0655291f24c3bc88297535822bc34d8a77
Patric Stout <truebrain@openttd.org>
2018-12-30 16:55:37 +0100

    Fix: capture SIGTERM correctly and propegate correctly
    As 'run.sh' is what is running the container, he will receive the
    SIGTERM. He should distribute it to the other parts of the code.

--- run.sh
+++ run.sh
@@ -3,15 +3,26 @@
 # Startup script for the container.
 #
 
+# When we get the TERM signal, forward it to deployer
+_term() {
+    if [ -n "${deployer_pid}" ]; then
+        kill ${deployer_pid}
+    fi
+    if [ -n "${tiller_pid}" ]; then
+        kill ${tiller_pid}
+    fi
+}
+trap _term INT TERM
+
 # Start a local tiller and make sure the rest connect to it
-TILLER_NAMESPACE=global tiller --history-max 10 --storage secret &
+TILLER_NAMESPACE=global linux-amd64/tiller --history-max 10 --storage secret &
 tiller_pid=$!
 sleep 1
 
-HELM_HOST=":44134" python -m deployer
-exit_code=$?
+# Start the deployer
+HELM_HOST=":44134" python -m deployer &
+deployer_pid=$!
+wait "${deployer_pid}"
 
 # If the deployer stops, stop tiller too
 kill ${tiller_pid}
-
-exit ${exit_code}
